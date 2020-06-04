d236209f83a37c98195987414bc8ee4d68242b7f
Kraze <Kraze@PHANTEKS>
2020-06-04 17:02:03 +0200

    clases

--- proyectoFinal1DAM/src/main/java/Main.java
+++ proyectoFinal1DAM/src/main/java/Main.java
@@ -1,3 +1,10 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,7 +21,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/institutos","root", "root");
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
